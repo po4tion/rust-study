727bcf3c5fd428b22faf5370847bab913457fd67
po4tion <po4tion0429@gmail.com>
2024-08-03 16:47:40 +0900

    variables

--- variables/src/main.rs
+++ variables/src/main.rs
@@ -22,4 +22,24 @@ fn main() {
   }
 
   println!("y의 값은 {y}입니다");
+
+  let tuples = (2, true, 1.14);
+  let (a, b, c) = tuples;
+  let first = tuples.0;
+  let second = tuples.1;
+  let third = tuples.2;
+
+  println!("tuples의 구조 분해 값은 {a} {b} {c}입니다");
+  println!(
+    "tuples의 구조 분해 값은 {0} {1} {2}입니다",
+    tuples.0, tuples.1, tuples.2
+  );
+
+  let arrays = [1, 2, 3, 4, 5];
+  println!("array의 첫번째 인덱스 값은 {0}입니다", arrays[0]);
+
+  let threes = [3; 100];
+  let last = threes[99];
+
+  println!("{last}");
 }
